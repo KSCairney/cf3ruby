062fbee5cf2b07333c3f1540c90ba77f380a8e95
KSCairney <kscairney@gmail.com>
2017-10-04 23:00:06 +0000

    5.2 fixed the forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
     def self.search(search_term)
-        Product.where("name LIKE ?", "%#{search_term}%")
+        if Rails.env.development?
+            Product.where("name LIKE ?", "%#{search_term}%")
+        elsif Rails.env.production?
+            Product.where("name ILIKE ?", "%#{search_term}%")
+        end
     end
 end
