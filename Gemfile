93057e7948b9a34b5ddcfc015ca32aeb31938966
KSCairney <kscairney@gmail.com>
2017-10-20 22:33:03 +0000

    6.3 rspec controller

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ gem 'will_paginate', '~> 3.1.0'
 group :development, :test do
   gem 'rspec-rails', '~> 3.6'
 end
+
+gem 'rails-controller-testing'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+    before do
+        @user = User.create!(email: "user@test.com", password: "testing")
+        @user2 = User.create!(email: "user2@test.com", password: "rabbit")
+    end
+        
+    describe 'GET #show' do
+        context 'when a user is logged in' do
+            before do
+                sign_in @user
+            end
+            
+            it 'loads the correct user details' do
+                get :show, :id => @user.id
+                expect(assigns(:user)).to eq @user
+                expect(response).to have_http_status(200)
+            end
+        end
+        
+        context 'user cannot see other users page' do
+            before do
+                sign_in @user2
+            end
+            
+            it 'does not load detail when another is logged in' do
+                get :show, :id => @user.id
+                expect(response).to redirect_to(root_path)
+                expect(response).to have_http_status(302)
+            end
+        end
+     
+        context 'when a user is not logged in' do
+            it 'redirects to login' do
+                get :show, :id => @user.id
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+    end
+end
+
