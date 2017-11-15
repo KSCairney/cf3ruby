dd45e5342befa2de542f5ee2368daf9110c227d0
KSCairney <kscairney@gmail.com>
2017-11-15 22:06:58 +0000

    6.14 testing advanced actionjob

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,7 @@ class Comment < ActiveRecord::Base
     presence: true
   validates :rating,
     numericality: { only_integer: true }
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
   belongs_to :user
   belongs_to :product
   scope :rating_desc, -> { order(rating: :desc) }
